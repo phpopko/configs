c978f6da282078f544cbe5072e6bab03a4168a4f
Philip Popko <ppopko03@gmail.com>
2026-02-02 15:18:45 -0500

    Update keymap.lua

--- neovim/lua/conf/keymap.lua
+++ neovim/lua/conf/keymap.lua
@@ -1,99 +1,107 @@
 local opts = { noremap = true, silent = true }
-vim.opt.clipboard = "unnamedplus"
 local term_opts = { silent = true }
 
+-- =========================
+-- Clipboard
+-- =========================
+vim.opt.clipboard = "unnamedplus"
 
-vim.cmd([[ cabbrev term sp <Bar> term ]])
-
-
--- Shorten function name
+-- =========================
+-- Helper for keymaps
+-- =========================
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
+-- =========================
+-- Leader key
+-- =========================
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
--- Normal --
--- Better window navigation
+-- =========================
+-- Normal mode keymaps
+-- =========================
+-- Window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+-- Scrolling with centering
 keymap("n", "<C-d>", "<C-d>zz", opts)
 keymap("n", "<C-u>", "<C-u>zz", opts)
 
---keymap("n", "<leader>e", ":Lex 30<cr>", opts)
-
--- Resize with arrows
+-- Resize windows
 keymap("n", "<C-Up>", ":resize +1<CR>", opts)
 keymap("n", "<C-Down>", ":resize -1<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -1<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +1<CR>", opts)
 
--- Navigate buffers
+-- Buffer navigation
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Insert --
--- Press jk fast to enter
--- keymap("i", "jk", "<ESC>", opts)
-
--- Visual --
--- Stay in indent mode
--- keymap("v", "<", "<gv", opts)
--- keymap("v", ">", ">gv", opts)
-
+-- Disable Ex mode
 keymap("n", "Q", "<nop>", opts)
 
--- Move text up and down
+-- =========================
+-- Visual mode keymaps
+-- =========================
+-- Move selected text
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
--- Visual Block --
--- Move text up and down
+-- =========================
+-- Visual Block mode
+-- =========================
 keymap("x", "J", ":move '>+1<CR>gv=gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv=gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv=gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv=gv", opts)
 keymap("x", "p", '"_dP', opts)
 
--- Terminal --
--- Better terminal navigation
+-- =========================
+-- Terminal mode keymaps
+-- =========================
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
-
--- Toggle file tree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-
--- (optional) Focus tree if already open
-keymap("n", "<leader>E", ":NvimTreeFocus<CR>", { silent = true })
-
--- (optional) Reveal current file in tree
-keymap("n", "<leader>f", ":NvimTreeFindFile<CR>", { silent = true })
-
-keymap("n", "<C-c>", '"+yy', opts)      -- copy line in normal mode
-keymap("v", "<C-c>", '"+y', opts)       -- copy selection in visual mode
-keymap("x", "<C-c>", '"+y', opts)       -- copy selection in visual-block mode
-
-keymap("n", "<C-v>", '"+p', opts)       -- paste after cursor in normal mode
-keymap("v", "<C-v>", '"+p', opts)       -- paste over selection (will replace)
-keymap("x", "<C-v>", '"+p', opts)       -- paste in visual-block mode
-
-keymap("i", "<C-v>", "<C-r>+", opts)    -- paste in insert mode
-
 keymap("t", "<C-w><Esc>", [[<C-\><C-n>]], opts)
 
+-- =========================
+-- NvimTree
+-- =========================
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+keymap("n", "<leader>E", ":NvimTreeFocus<CR>", { silent = true })
+keymap("n", "<leader>f", ":NvimTreeFindFile<CR>", { silent = true })
+
+-- =========================
+-- Clipboard shortcuts
+-- =========================
+-- Normal / Visual / Visual-Block
+keymap("n", "<C-c>", '"+yy', opts)
+keymap("v", "<C-c>", '"+y', opts)
+keymap("x", "<C-c>", '"+y', opts)
+
+keymap("n", "<C-v>", [[<Esc>:set paste<CR>"+gP:set nopaste<CR>]], opts)
+keymap("i", "<C-v>", [[<Esc>:set paste<CR>"+gP:set nopaste<CR>i]], opts)
+keymap("v", "<C-v>", '"+p', opts)
+
+-- Terminal mode clipboard
+keymap("t", "<C-c>", [[<C-\><C-N>"+y]], term_opts)
+keymap("t", "<C-v>", [[<C-\><C-N>:set paste<CR>"+gP:set nopaste<CR>i]], term_opts)
+
+
+vim.api.nvim_create_user_command("Term", function()
+  vim.cmd("vsplit")                                -- right vertical split
+  vim.cmd("lcd /mnt/c/Users/ppopk/prog")          -- fixed folder
+  vim.cmd("terminal")                              -- open terminal
+  vim.cmd("startinsert")                           -- go to insert mode
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+end, {})
+
+-- Optional fast terminal keymap
+vim.keymap.set("n", "<leader>t", ":Term<CR>", { silent = true })
